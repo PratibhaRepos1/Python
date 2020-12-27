13a7b916ee880c68d7b891468b716c98102f074c
Pratibha <pratibha.jadhav3229@gmail.com>
2020-12-27 13:38:00 +0530

    maths practice work

---
+++ Basics/maths_practice.py
@@ -0,0 +1,41 @@
+age = 34
+print(age)
+mypi = 3.14
+print(mypi)
+
+# Doing Maths with Python
+# find out area of the circle
+pi = 3.14
+radius = 5
+area = pi * radius * radius
+print(area)
+#another way
+area = pi * radius ** 2
+print(area)
+
+#addition
+sum = 10 + 20
+print(sum)
+#substraction
+substraction = 20-5
+print(substraction)
+#division
+div = 5/3
+print(div)
+
+#division
+div = 5//3
+print(div)
+
+print(2**2)
+print(2**3)
+print(2**4)
+print(2**5)
+
+weeks = round(365.25 / 7, 1)
+print(weeks)
+
+#conversion
+
+intNumber = int(34.56)
+print(intNumber)
